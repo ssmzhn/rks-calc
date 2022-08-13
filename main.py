a04c9c67b8e88feacfaaa32e1c7c53d20e415a2d
ssmzhn <ssmzhn@163.com>
2022-08-13 20:15:18 +0800

    加了adb

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import unpack  # 解包 .ab 和 .tar
 import replace # 解码存档
 import calc    # 计算 rks
-def main():
-    print('欢迎使用 rks 计算器! ')
-    file = input('请输入 adb 备份后的 .ab 文件绝对路径: ')
+import os
+import sys
+def adb_extract(filename):
+    if os.system('adb version') != 0:
+        return -1
+    if os.name == 'posix':
+        os.system('sudo adb start-server')
+    else:
+        os.system('adb start-server')
+    if os.system('adb backup -f {} com.PigeonGames.Phigros'.format(filename)) != 0:
+        return -2
+    return filename
+
+def main(file):
     data_dir = unpack.unpack(file)
     xml_file = "{data_dir}{separate}apps{separate}com.PigeonGames.Phigros{separate}sp{separate}com.PigeonGames.Phigros.v2.playerprefs.xml".format(data_dir=data_dir,separate=unpack.separate)
     score = replace.get_score(score_source_file=xml_file)
@@ -19,4 +30,32 @@ def main():
         print('     ACC: {} 单曲RKS: {}'.format(x['acc'],x['rks']))
         i+=1
 if __name__ == '__main__':
-    main()
+    print('欢迎使用 rks 计算器! ')
+    print('    (1) 已有 *.ab 备份文件，直接计算')
+    print('    (2) 没有 *.ab 备份文件，备份后计算')
+    choice = 0
+    while True:
+        choice = input('请选择 ( 1 或 2 ): ')
+        if choice in ('1','2'):
+            break
+        print('输入有误，请重新输入: ')
+    if choice == '1':
+        main(input('请输入 adb 备份后的 .ab 文件绝对路径: '))
+    elif choice == '2':
+        file = input('请输入存档的保存位置 (绝对路径): ')
+        adb_code = adb_extract(file)
+        if adb_code==-1:
+            print('未找到 ADB 程序! 请下载 ADB 软件包并配置环境变量。')
+            sys.exit(-1)
+        elif adb_code==-2:
+            print('提取错误! 可能有下列原因: ')
+            print('    1. 未安装 Phigros;')
+            print('    2. 驱动未安装、手机未连接或未打开 USB 调试;')
+            print('    3. 无法写入文件，可能是因为保存位置的上级目录不存在;')
+            print('    4. 未正确打开 ADB 服务，这可能是没有用管理员或 root 打开 adb;')
+            print('    5. 其他杂七杂八的问题。')
+            sys.exit(-2)
+        else:
+            main(file)
+
+
--- unpack.py
+++ unpack.py
@@ -1,15 +1,17 @@
 import os
 import tarfile
-current_dir=os.getcwd()
+filedir=os.getcwd()
 separate=""
 if os.name=="nt":
     separate = "\\"
 else:
     separate = "/"
 def unpack(filename):
-    unpacked_name=filename.split(separate)[-1].split('.',1)[0]
-    os.system('java -jar abe.jar unpack {} {}'.format(filename,current_dir+separate+unpacked_name+'.tar'))
-    os.system('mkdir '+current_dir+separate+unpacked_name)
-    file = tarfile.open(current_dir+separate+unpacked_name+'.tar')
-    file.extractall(path=current_dir+separate+unpacked_name)
-    return current_dir+separate+unpacked_name
+    filedir = os.path.split(filename)[0]
+    unpacked_name='.'.join(filename.split(separate)[-1].split('.')[:-1])
+    print('java -jar abe.jar unpack {} {}'.format(filename,filedir+separate+unpacked_name+'.tar'))
+    os.system('java -jar abe.jar unpack {} {}'.format(filename,filedir+separate+unpacked_name+'.tar'))
+    os.system('mkdir '+filedir+separate+unpacked_name)
+    file = tarfile.open(filedir+separate+unpacked_name+'.tar')
+    file.extractall(path=filedir+separate+unpacked_name)
+    return filedir+separate+unpacked_name
